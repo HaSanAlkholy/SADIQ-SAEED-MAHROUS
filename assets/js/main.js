6f299737b338b2c808ff0064d253009a97fd2606
Hasan <hassan.m.h.m.a123@gmail.com>
2020-09-29 16:24:19 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,284 @@
+$(window).on('load', function () {
+
+  //preloader
+  $('.pre-loader').fadeOut("500", function () {
+    $('html').removeClass("overflow");
+    $(this).remove();
+  });
+
+  //nava toggle
+  $('#nava-icon').click(function (e) {
+    $('#nava').toggleClass('nava-active');
+    $('html').toggleClass('overflow');
+  });
+
+  $('#nava').click(function (e) {
+    if (e.target.id == 'nava' || e.target.id == 'close-nava' || e.target.parentNode.id == "close-nava") {
+      $(this).removeClass('nava-active');
+      $('html').removeClass('overflow');
+    }
+  });
+
+  //search toggle
+  $('#search-btn').click(function (e) {
+    $('#search-form').toggleClass('search-form-active');
+    $('html').toggleClass('overflow');
+  });
+
+  $('#search-form').click(function (e) {
+    if (e.target.id == 'search-form' || e.target.id == 'close-search' || e.target.parentNode.id == "close-search") {
+      $(this).removeClass('search-form-active');
+      $('html').removeClass('overflow');
+    }
+  });
+
+  $(".slide").on("click", function (e) {
+    if (e.target.classList.contains('drop')) {
+      console.log(e.target);
+      $(this).toggleClass("slide-active");
+      $(this).children("ul").slideToggle();
+    }
+  });
+
+
+  var mySwiper = new Swiper('header .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    autoplay: {
+      delay: 5000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var mySwiper2 = new Swiper('.s-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 5000,
+    },
+    breakpoints: {
+      // when window width is >= 480px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 250px
+      150: {
+        slidesPerView: 1,
+      }
+    },
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var mySwiper3 = new Swiper('#brands .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    slidesPerView: 6,
+    spaceBetween: 10,
+    autoplay: false,
+    allowTouchMove: false,
+
+    breakpoints: {
+      1200: {
+        slidesPerView: 6,
+      },
+      992: {
+        slidesPerView: 5,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      768: {
+        slidesPerView: 4,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      576: {
+        slidesPerView: 3,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      // when window width is >= 250px
+      0: {
+        slidesPerView: 2,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      }
+    },
+
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  var mySwiper4 = new Swiper('.notes-swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    slidesPerView: 4,
+    autoplay: false,
+    allowTouchMove: false,
+
+    breakpoints: {
+      // when window width is >= 576px
+      1200: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 3,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      576: {
+        slidesPerView: 2,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      // when window width is >= 250px
+      0: {
+        slidesPerView: 1,
+        allowTouchMove: true,
+        autoplay: 4000,
+      }
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  });
+
+  var mySwiper5 = new Swiper('footer .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    slidesPerView: 6,
+    spaceBetween: 10,
+    autoplay: false,
+    allowTouchMove: false,
+
+    breakpoints: {
+      1200: {
+        slidesPerView: 6,
+      },
+      992: {
+        slidesPerView: 5,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      768: {
+        slidesPerView: 4,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      576: {
+        slidesPerView: 3,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      },
+      // when window width is >= 250px
+      0: {
+        slidesPerView: 2,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 4000,
+        }
+      }
+    },
+
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+
+
+  if (window.innerWidth <= 1200) {
+    setInterval(() => {
+      mySwiper3.slideNext();
+      mySwiper4.slideNext();
+      mySwiper5.slideNext();
+    }, 4000);
+  }
+
+  // animate on scroll
+
+  AOS.init({
+    duration: 700,
+    mirror: true,
+    once: true,
+    disable: function () {
+      var maxWidth = 769;
+      return window.innerWidth < maxWidth;
+    },
+    easing: 'ease-in-out'
+  });
+
+
+
+});
